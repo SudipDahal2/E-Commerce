67200f55bcaf2569e42ebdec18a07a822e5406f5
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-01-06 10:31:55 +0545

    02 auth controller created by Sujan

---
+++ E_commerce/Backend/src/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+// import {User} from "../models/user.model.js"
+
+export const signup=async(req,res)=>{
+    console.log("Signup route")
+}
+
+export const login=async(req,res)=>{
+    console.log("Login route")
+}
+
+export const logout=async(req,res)=>{
+    console.log("Logout route")
+}
+
+export const getProfile=async(req,res)=>{
+    console.log("Profile route")
+}
